bf0c050675bbf9e83ecbf73e67706add272fad64
Daniel Mitre <daniel.mitre@ccc.ufcg.edu.br>
2019-12-04 10:16:34 -0300

    ooops

--- parser.py
+++
@@ -1,7 +0,0 @@
-import json
-
-def parse_ndjson(file_path):
-    """ Parse ndjson files """
-    contents = open(file_path, "r").read() 
-    data = [json.loads(str(item)) for item in contents.strip().split('\n')]
-    return data
