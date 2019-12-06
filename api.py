f940fc161d881380d9d44c92f827283bf914d528
Héricles Emanuel <hericles.me@gmail.com>
2019-12-06 04:53:58 -0300

    demo n' trained model

--- api.py
+++ api.py
@@ -1,26 +1,35 @@
-import flask
-import tensorflow as tf
-from PIL import Image
+import tensorflow as tf 
 import keras
-from keras.models import load_model
+import cv2
+import numpy as np
 
-app = flask.Flask(__name__)
+import base64
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
-model = load_model('gartic.hdf5')
+
+app = Flask(__name__)
+CORS(app)
 
 @app.route("/predict", methods=["GET","POST"])
 def predict():
     data = {"success": False}
+    img = request.get_json()
+    starter = img.find(',')
+    image_data = img[starter+1:]
+    decoded = base64.b64decode(image_data)
 
-    params = flask.request.json
-    if (params == None):
-        params = flask.request.data
+    with open('image.jpg', 'wb') as fh:
+        fh.write(decoded)
+        fh.close()
+    data["success"] = True
+    classifierLoad = tf.keras.models.load_model('gartic.hdf5') 
+    image = cv2.imread('image.jpg')
+    small_image = cv2.resize(image, (28, 28), interpolation=cv2.INTER_CUBIC)
+    small_image, _, _ = cv2.split(small_image)
+    data['index'] = (classifierLoad.predict_classes(np.expand_dims([small_image], axis=-1)).tolist()[0])
 
-    if (params != None):
-        np_img = np.array(params, dtype='uint8')
-        data["prediction"] = str(model.predict(np_img)[0][0])
-        data["success"] = True
 
-    return flask.jsonify(data)    
+    return jsonify(data)    
 
 app.run(host='0.0.0.0', port=5000)
--- static/js/quickdraw.js
+++ static/js/quickdraw.js
@@ -1,18 +1,42 @@
-(function()
-{
-	var canvas = document.querySelector( "#canvas" );
-	var context = canvas.getContext( "2d" );
+
+let class_names = ["abelha", "livro", "cacto", "cisne", "submarino", "relógio", "tigre", "torradeira", "cueca", "guarda-chuva"];
+
+(() => {
+	let canvas = document.querySelector( "#canvas" );
+	let context = canvas.getContext( "2d" );
 	canvas.width = 280;
 	canvas.height = 280;
 
-	var Mouse = { x: 0, y: 0 };
-	var lastMouse = { x: 0, y: 0 };
+	let Mouse = { x: 0, y: 0 };
+	let lastMouse = { x: 0, y: 0 };
 	context.fillStyle="white";
 	context.fillRect(0,0,canvas.width,canvas.height);
 	context.color = "black";
-	context.lineWidth = 20;
+	context.lineWidth = 15;
     context.lineJoin = context.lineCap = 'round';
-	
+
+
+	const debug = () => {
+		let clearButton = $( "#clearButton" );
+		
+		clearButton.on( "click", () => {
+				context.clearRect( 0, 0, 280, 280 );
+				context.fillStyle="white";
+				context.fillRect(0,0,canvas.width,canvas.height);
+		});
+
+
+		$( "#colors" ).change(() => {
+			let color = $( "#colors" ).val();
+			context.color = color;
+		});
+		
+		
+		$( "#lineWidth" ).change(() => {
+			context.lineWidth = $( this ).val();
+		});
+	}
+
 	debug();
 
 	canvas.addEventListener( "mousemove", function( e )
@@ -37,8 +61,7 @@
 
 	}, false );
 
-	var onPaint = function()
-	{	
+	let onPaint = function() {	
 		context.lineWidth = context.lineWidth;
 		context.lineJoin = "round";
 		context.lineCap = "round";
@@ -50,41 +73,7 @@
 		context.closePath();
 		context.stroke();
 	};
-
-	function debug()
-	{
-		/* CLEAR BUTTON */
-		var clearButton = $( "#clearButton" );
-		
-		clearButton.on( "click", function()
-		{
-			
-				context.clearRect( 0, 0, 280, 280 );
-				context.fillStyle="white";
-				context.fillRect(0,0,canvas.width,canvas.height);
-
-				myChart.data.labels = [];
-				myChart.data.datasets[0].data = [];
-				myChart.update();
-
-		});
-
-		/* COLOR SELECTOR */
-
-		$( "#colors" ).change(function()
-		{
-			var color = $( "#colors" ).val();
-			context.color = color;
-		});
-		
-		/* LINE WIDTH */
-		
-		$( "#lineWidth" ).change(function()
-		{
-			context.lineWidth = $( this ).val();
-		});
-	}
-}());
+})();
 
 
 function hideCollage(){
@@ -95,13 +84,69 @@ $(function(){
 	hideCollage();
 });
 
+var mousePress = false;
 
-$("span").click(function(){
-	e = document.getElementById('slider')
-	if (e.style.display == 'none'){
-		$("#slider").slideDown(1000);
+canvas.onmousedown = function (e) {
+	mousePress = true;
+	getPred();
 }
-	else{
-		$("#slider").slideUp(1000);
+
+canvas.onmouseup = function (e) {
+	mousePress = false;
+	getPred();
+}
+
+function getPred() {
+	var label = document.getElementById('label');
+	var $SCRIPT_ROOT = "http://localhost:5000";
+	var canvasObj = document.getElementById("canvas");
+	var img = canvasObj.toDataURL();
+	$.ajax({
+		type: "POST",
+		dataType : 'json',
+		contentType: 'application/json',
+		url: $SCRIPT_ROOT + "/predict",
+		data: JSON.stringify(img),
+		success: function (data) {
+			label.innerText = class_names[data.index]
+		}
+	});
+}
+
+
+var counter = 0;
+
+function timer() {
+	if (counter < 4) {
+		counter = counter + 1;
 	}
-})
+	else {
+		counter = 0
+	}
+}
+
+setInterval(timer, 100);
+
+document.getElementById("canvas").onmousemove = function (e) {
+	if (!mousePress) return;
+
+	while (mousePress) {
+		if (counter == 3) {
+			getPred();
+		}
+
+
+		return false;
+	}
+
+};
+
+	
+let categories = document.getElementById('categories');
+
+class_names.map(e => {
+	let node = document.createElement("LI");
+	let textnode = document.createTextNode(e);
+	node.appendChild(textnode); 
+	categories.appendChild(node)
+});
